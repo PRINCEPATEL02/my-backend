4eca3d40d50f299912b72436cc3a681b9288b9de
PRINCEPATEL02 <princespatel02@gmail.com>
2025-07-18 18:30:50 +0530

    Updated login logic and fixed bug in dashboard

--- config.js
+++ config.js
@@ -11,5 +11,5 @@ module.exports = {
     "mongodb+srv://Princesp02:Princesp02@cluster0.tykleyq.mongodb.net/pitchboard?retryWrites=true&w=majority",
 
   PORT: process.env.PORT || 5000,
-  JWT_SECRET: process.env.JWT_SECRET || "your-secret-key",
+  JWT_SECRET: process.env.JWT_SECRET || "9e65643cc8258d9ea56338a1ef8a721a93fbc4b3a83ccdd19c46b99eb56ee193a5977dc37a6cf25b1178d157a1a1f812b044240215839ba83245c00ae0f64fb5",
 };
